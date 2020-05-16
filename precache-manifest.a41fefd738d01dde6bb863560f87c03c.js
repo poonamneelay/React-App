00504f5beefe9a446745d6a536cc904d45fec25e
tbidwa401 <tejas_bidwai@comcast.com>
2020-05-16 07:41:14 -0400

    Updates

---
+++ precache-manifest.a41fefd738d01dde6bb863560f87c03c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e004ce67b2e7952db6322a4621d6c1",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "4555578c23466786499e",
+    "url": "/React-App/static/css/main.3833ba2f.chunk.css"
+  },
+  {
+    "revision": "d7556712cc843c15ab50",
+    "url": "/React-App/static/js/2.0acf6400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.0acf6400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4555578c23466786499e",
+    "url": "/React-App/static/js/main.341b53af.chunk.js"
+  },
+  {
+    "revision": "2d57dce4cc13a630995f",
+    "url": "/React-App/static/js/runtime-main.bbb55fe2.js"
+  }
+]);
